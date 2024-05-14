c082311640039215359733ca8a07a401085732d5
StevenDiep <sdiep24@yahoo.com>
2024-05-14 17:06:46 -0500

    Finished the entire project's rough draft, will have to check the prompt to handle edge cases

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Thoughts: Going to attempt this project in a 3-step process. First, I'm going to
 Part one: Setting up the grid was smooth, though you have to create a new div for each individual grid square (thought you could get away with just creating one and appending x amount of times to save time). Initially flexed the body but you should be able to just flex the main container if the dimensions are set to the webpage.
 
 Part two: Initially, I used querySelector(".grid") hoping that it would identify all of the squares in the class. However, the approach that worked in the end was using the querySelectorAll method and then using a for loop to loop through each individual square and add eventListeners to all of them. The approach works, though it seems slow and I'm wondering if there's a better solution out there. The type of event listener is mouseleave and I still need to wrap my head around the syntax for (event) =>)
+
+Part three: Had to change up the createSquareDiv function to include div.style.setProperty('width', `calc(960px/${gridSize})`) so that the pixels used would stay the same regardless of the input. Not sure if I reset the board correctly. I used mainDiv.innerHTML = "" which works, but not sure if that is the optimal solution. Will also have to go back and fix up the user prompt to handle incorrect input. 
+
+Going forward: Make sure you did part two correctly with the for loop. Then look into making sure you did part three optimally with innerHTML = "". Then make sure the user input for the prompt handles the edge cases well.
--- javascript.js
+++ javascript.js
@@ -1,29 +1,53 @@
+function createSquareDiv(gridSize){
+    const div = document.createElement("div");
+    div.style.backgroundColor = "black";
+    div.style.color = "blue";
+    div.style.setProperty('width', `calc(960px/${gridSize})`)
+    div.style.setProperty('height', `calc(600px/${gridSize})`)
+    div.classList.add("grid");
+
+    return div
+}
+function addHoverEffect(){
+    let squareList = document.querySelectorAll(".grid")
+    for (let i =0;i< squareList.length;i++){
+        squareList[i].addEventListener("mouseleave", (event)=>{
+            squareList[i].style.backgroundColor = "yellow";
+        });
+    }
+}
 function createGrid(mainDiv, gridSize){
     for (let i = 0; i<gridSize; i++){
         const rowDiv = document.createElement("div")
         rowDiv.classList.add("row")
 
         for (let j = 0; j<gridSize; j++){
-            const div = document.createElement("div");
-            div.style.backgroundColor = "black";
-            div.style.color = "blue";
-            div.classList.add("grid");
+            const div = createSquareDiv(gridSize)
             rowDiv.appendChild(div)
         }
         mainDiv.appendChild(rowDiv)
-        
     }
+    addHoverEffect()
+}
+
+function resetBoard(){
+    let gridSize = prompt("Please enter a grid size", 64);
+    if (gridSize == null || gridSize == ""){
+        gridSize = 64;
+    }
+    else if (gridSize > 100){
+        gridSize = 100;
+    }
+
+    const mainDiv = document.querySelector("#mainDiv");
+    mainDiv.innerHTML = "";
+
+    createGrid(mainDiv, gridSize);
 
 
 }
-
 const container = document.querySelector("#mainDiv");
 createGrid(container, 16)
 
-let squareList = document.querySelectorAll(".grid")
-for (let i =0;i< squareList.length;i++){
-    squareList[i].addEventListener("mouseleave", (event)=>{
-        squareList[i].style.backgroundColor = "yellow";
-    });
-}
+
 
